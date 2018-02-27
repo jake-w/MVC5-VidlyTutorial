ea74f9fc83e9b6321716204ca6523a954eb50cee
jake-w <jake_mw@yahoo.com>
2018-02-27 18:12:27 +1300

    ByReleaseDate route

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -16,5 +16,11 @@ namespace Vidly.Controllers
 
             return View(movie);
         }
+
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
     }
 }
